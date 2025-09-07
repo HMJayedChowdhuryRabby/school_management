e9a65ad7b4212ee4937767d67b6c43b32d105ea5
JayedChowdhuryRabby <19-40893-2@student.aiub.edu>
2025-09-08 00:51:05 +0600

    nine

--- src/app.js
+++ src/app.js
@@ -39,6 +39,7 @@ app.use('/uploads', (req, res, next) => {
 
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", "true");
   }
 
   res.header("Access-Control-Allow-Methods", "GET,OPTIONS");
